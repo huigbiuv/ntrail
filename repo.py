7dadf9ac3c66a39df1eade75e4e03e71839bed14
huigbiuv <108453186+huigbiuv@users.noreply.github.com>
2022-07-01 15:42:13 +0530

    Update repo.py

--- repo.py
+++ repo.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np 
 from PIL import Image
 #import plotly.express as px
-from plotly.subplots import make_subplots
+#from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 from fbprophet import Prophet
 from fbprophet.plot import plot_plotly
